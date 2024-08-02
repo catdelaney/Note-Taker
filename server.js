480915f31bac6f0e3cc70bf1a6e4d1f98683eaa0
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-02 14:13:02 -0230

    edits

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use('/api/notes', routes);
 
 // HTML routes
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
 );
 
 app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 );
 
 app.listen(PORT, () => 
