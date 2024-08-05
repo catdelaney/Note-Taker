79c4d51fe35c7685a6b9688b7a699129c74b41f6
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-05 19:50:09 -0230

    server.js edits

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.use('/api/notes', routes);
+app.use('/api', routes);
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'develop/public/index.html'))
 );
 
-app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
 );
 
 app.listen(PORT, () => 
