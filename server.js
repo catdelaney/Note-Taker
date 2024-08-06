231b27899f9738da3978cf4fb7ff7cba4c31aeba
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-05 21:32:59 -0230

    testing

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
