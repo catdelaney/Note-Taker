d1ee5b70b00b043031a7314a46f3fb52b14f43d0
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-05 22:07:58 -0230

    api routes testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const dbPath = path.join(__dirname, '../db/db.json')
 
 module.exports = (app) => 
     app.get('/api/notes', (req, res) => {
-        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-            if (err) return res.status(500).json({ error: err.message });
-            res.json(JSON.parse(data));
+        fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes' });
+        }
+        res.json(JSON.parse(data));
     });
   });
 
   app.post('/api/notes', (req, res) => {
     const newNote = { ...req.body, id: uuidv4() };
 
-    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-      if (err) return res.status(500).json({ error: err.message });
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to read notes.' });
+      }
       const notes = JSON.parse(data);
       notes.push(newNote);
 
-      fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-        if (err) return res.status(500).json({ error: err.message });
+      fs.writeFile(dbPath, JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to save note.'});
+        }
         res.json(newNote);
       });
     });
@@ -28,13 +38,16 @@ module.exports = (app) =>
   app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
 
-    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-      if (err) return res.status(500).json({ error: err.message });
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) return res.status(500).json({ error: 'Failed to read notes.' });
       let notes = JSON.parse(data);
       notes = notes.filter(note => note.id !== noteId);
 
       fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-        if (err) return res.status(500).json({ error: err.message });
+        if (err) {
+            console.error(err); 
+            return res.status(500).json({ error: 'Failed to delete note' });
+        }
         res.json({ message: 'Note deleted successfully' });
       });
     });
