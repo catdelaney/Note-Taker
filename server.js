be851b002598a0acb5cc3647de85ca0c24d5cce2
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-07 10:43:35 -0230

    notes showing up

--- server.js
+++ server.js
@@ -8,25 +8,26 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded());
 
 // HTML Routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('db.json', 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading notes:', err);
             res.status(500).json({ error: 'Failed to read notes' });
         } else {
             console.log('Notes read successfully', data);
-            res.json((JSON.parse(data)).notes);
+            res.json((JSON.parse(data)));
         }
     });
 });
